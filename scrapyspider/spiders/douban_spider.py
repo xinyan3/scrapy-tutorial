acdd19869e0aec507768c2c742dfe4d92fb244ac
test <sky831202@gmail.com>
2018-02-04 16:30:13 +0800

    Update douban_spider.py

--- scrapyspider/spiders/douban_spider.py
+++ scrapyspider/spiders/douban_spider.py
@@ -30,7 +30,7 @@ class DoubanMovieTop250Spider(Spider):
                 './/div[@class="star"]/span[@class="rating_num"]/text()'
             ).extract()[0]
             item['score_num'] = movie.xpath(
-                './/div[@class="star"]/span/text()').re(ur'(\d+)人评价')[0]
+                './/div[@class="star"]/span[4]/text()').extract()[0]
             yield item
 
         next_url = response.xpath('//span[@class="next"]/a/@href').extract()
